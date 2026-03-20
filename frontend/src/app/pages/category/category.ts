1273e47e89b21f9332f64c7d8a339c731100ae99
IllonaSab <sabouillona@gmail.com>
2026-03-20 12:01:44 +0100

    feat: category

--- frontend/src/app/pages/category/category.ts
+++ frontend/src/app/pages/category/category.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApolloService } from '../../services/apollo';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './category.html',
   styleUrl: './category.scss',
 })
-export class Category {}
+export class Category implements OnInit {
+  articles: any[] = [];
+  categoryName: string = '';
+  categorySlug: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private apolloService: ApolloService,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.categorySlug = params['slug'];
+      this.apolloService.getArticlesByCategory(this.categorySlug).subscribe((articles) => {
+        this.articles = articles;
+        if (articles.length > 0) {
+          this.categoryName = articles[0].category?.nom;
+        }
+      });
+    });
+  }
+}
