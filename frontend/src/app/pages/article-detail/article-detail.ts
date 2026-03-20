e1240e832f50bf08648fa96649de3419f4ee712f
IllonaSab <sabouillona@gmail.com>
2026-03-20 11:54:41 +0100

    feat: componants Header-Home

--- frontend/src/app/pages/article-detail/article-detail.ts
+++ frontend/src/app/pages/article-detail/article-detail.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApolloService } from '../../services/apollo';
+
+import { RichTextPipe } from '../../pipes/rich-text-pipe';
 
 @Component({
   selector: 'app-article-detail',
-  imports: [],
+  imports: [RouterLink, CommonModule, RichTextPipe],
   templateUrl: './article-detail.html',
   styleUrl: './article-detail.scss',
 })
-export class ArticleDetail {}
+export class ArticleDetail implements OnInit {
+  article: any = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private apolloService: ApolloService,
+  ) {}
+
+  ngOnInit() {
+    const slug = this.route.snapshot.paramMap.get('slug');
+    if (slug) {
+      this.apolloService.getArticleBySlug(slug).subscribe((article) => {
+        this.article = article;
+      });
+    }
+  }
+}
