d8d7b9ea434b4b43ec121216c41fe80fd36994b6
IllonaSab <sabouillona@gmail.com>
2026-03-20 11:25:49 +0100

    feat: angular+appolo+components

---
+++ frontend/src/app/pages/article-detail/article-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-detail',
+  imports: [],
+  templateUrl: './article-detail.html',
+  styleUrl: './article-detail.scss',
+})
+export class ArticleDetail {}
---
+++ frontend/src/app/services/apollo.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApolloService {
+  constructor(private apollo: Apollo) {}
+
+  getArticles(): Observable<any[]> {
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          query {
+            articles {
+              documentId
+              titre
+              extrait
+              date
+              slug
+              category {
+                nom
+                slug
+              }
+            }
+          }
+        `,
+      })
+      .valueChanges.pipe(map((result: any) => result.data.articles));
+  }
+
+  getArticleBySlug(slug: string): Observable<any> {
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          query GetArticle($slug: String!) {
+            articles(filters: { slug: { eq: $slug } }) {
+              documentId
+              titre
+              contenu
+              extrait
+              date
+              slug
+              category {
+                nom
+                slug
+              }
+              metaTitle
+              metaDescription
+            }
+          }
+        `,
+        variables: { slug },
+      })
+      .valueChanges.pipe(map((result: any) => result.data.articles[0]));
+  }
+
+  getCategories(): Observable<any[]> {
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          query {
+            categories {
+              documentId
+              nom
+              slug
+            }
+          }
+        `,
+      })
+      .valueChanges.pipe(map((result: any) => result.data.categories));
+  }
+
+  getArticlesByCategory(slug: string): Observable<any[]> {
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          query GetArticlesByCategory($slug: String!) {
+            articles(filters: { category: { slug: { eq: $slug } } }) {
+              documentId
+              titre
+              extrait
+              date
+              slug
+              category {
+                nom
+                slug
+              }
+            }
+          }
+        `,
+        variables: { slug },
+      })
+      .valueChanges.pipe(map((result: any) => result.data.articles));
+  }
+}
